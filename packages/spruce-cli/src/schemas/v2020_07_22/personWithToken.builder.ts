eaddcad880050904ab9a2d90de2d067f62b0a4ea
Taylor Romero <me@taylorrome.ro>
2021-12-11 09:00:10 -0700

    lint fix

--- packages/spruce-cli/src/schemas/v2020_07_22/personWithToken.builder.ts
+++ packages/spruce-cli/src/schemas/v2020_07_22/personWithToken.builder.ts
@@ -1,4 +1,5 @@
 import { buildSchema } from '@sprucelabs/schema'
+// eslint-disable-next-line spruce/prohibit-import-of-schema-in-builders
 import personSchema from '#spruce/schemas/spruce/v2020_07_22/person.schema'
 
 export default buildSchema({
