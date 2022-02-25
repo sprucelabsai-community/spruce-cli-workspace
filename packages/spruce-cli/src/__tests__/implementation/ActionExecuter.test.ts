61b5be6cd6740d2c03b69e2b46802e3c6545b2ae
Taylor Romero <me@taylorrome.ro>
2022-02-24 21:43:35 -0700

    patch; import fixes

--- packages/spruce-cli/src/__tests__/implementation/ActionExecuter.test.ts
+++ packages/spruce-cli/src/__tests__/implementation/ActionExecuter.test.ts
@@ -1,6 +1,6 @@
-import * as coreSchemas from '@sprucelabs/spruce-core-schemas'
 import { versionUtil } from '@sprucelabs/spruce-skill-utils'
 import { test, assert } from '@sprucelabs/test'
+import { coreSchemas } from '../../features/schema/stores/SchemaStore'
 import AbstractSchemaTest from '../../tests/AbstractSchemaTest'
 
 export default class FeatureCommandExecuterTest extends AbstractSchemaTest {
