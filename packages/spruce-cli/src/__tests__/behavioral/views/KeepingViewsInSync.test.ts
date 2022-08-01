d4284d1c8a02a7ae387f3e9f597b3456cbe0353f
Taylor Romero <me@taylorrome.ro>
2022-08-01 13:42:43 -0600

    patch: syncing views with no skill views skill works!

--- packages/spruce-cli/src/__tests__/behavioral/views/KeepingViewsInSync.test.ts
+++ packages/spruce-cli/src/__tests__/behavioral/views/KeepingViewsInSync.test.ts
@@ -1,4 +1,5 @@
 import { test, assert } from '@sprucelabs/test'
+import { FeatureActionResponse } from '../../../features/features.types'
 import SyncAction from '../../../features/view/actions/SyncAction'
 import AbstractSkillTest from '../../../tests/AbstractSkillTest'
 import testUtil from '../../../tests/utilities/test.utility'
@@ -31,6 +32,21 @@ export default class KeepingViewsInSyncTest extends AbstractSkillTest {
 			isRoot: true,
 		})
 
+		await this.assertValuedViewsFile(results)
+	}
+
+	@test()
+	protected static async generatesValidTypesFileWithNoSkillViewsC() {
+		const results = await this.Action('view', 'create').execute({
+			viewType: 'view',
+			nameReadable: 'test',
+			viewModel: 'Card',
+		})
+		assert.isFalsy(results.errors)
+		await this.assertValuedViewsFile(results)
+	}
+
+	private static async assertValuedViewsFile(results: FeatureActionResponse) {
 		const match = testUtil.assertFileByPathInGeneratedFiles(
 			`views/views.ts`,
 			results.files
