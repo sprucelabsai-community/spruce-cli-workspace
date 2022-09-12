6fa6279ad39c06801b808ea05205db98f04cd070
Ryan Johnson <ryan@sprucelabs.ai>
2022-09-12 14:09:25 -0400

    patch: spruce upgrade (added spruce-event-utils to template upgrade)

--- upgrade.sh
+++ upgrade.sh
@@ -3,7 +3,7 @@ yarn add  @babel/cli @babel/plugin-proposal-decorators @babel/runtime @jest/repo
 yarn add  -D @sprucelabs/data-stores @sprucelabs/jest-sheets-reporter @sprucelabs/mercury-core-events @sprucelabs/resolve-path-aliases @sprucelabs/spruce-conversation-plugin @sprucelabs/spruce-deploy-plugin @sprucelabs/spruce-store-plugin @sprucelabs/spruce-test-fixtures @sprucelabs/test @sprucelabs/test-utils @types/blessed @types/eslint @types/fs-extra @types/inflection @types/jsonwebtoken @types/lodash @types/md5 @types/mkdirp @types/node @types/promise.allsettled @types/ps-node @types/rimraf @types/semver @types/sha1 @types/slug @types/superagent @types/terminal-kit @types/uuid chokidar-cli concurrently conventional-changelog-sprucelabs dotenv eslint eslint-config-spruce find-process jest jest-circus jest-junit jest-reporters prettier ps-node rimraf ts-jest ts-node tsc-watch tsconfig-paths 
 
 cd ../spruce-templates
-yarn add  @sprucelabs/mercury-types @sprucelabs/schema @sprucelabs/spruce-skill-utils handlebars lodash sha1
+yarn add  @sprucelabs/mercury-types @sprucelabs/schema @sprucelabs/spruce-event-utils @sprucelabs/spruce-skill-utils handlebars lodash sha1
 yarn add  -D @types/lodash @types/node chokidar-cli concurrently conventional-changelog-sprucelabs prettier typescript
 
 yarn rebuild
