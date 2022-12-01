5f2f7fa81e0079a7136e68944fdb61a6f66f0111
Taylor Romero <me@taylorrome.ro>
2022-11-30 20:44:15 -0700

    patch: accidental only

--- packages/spruce-cli/src/__tests__/behavioral/events/CreatingAListener.test.ts
+++ packages/spruce-cli/src/__tests__/behavioral/events/CreatingAListener.test.ts
@@ -203,7 +203,7 @@ export default class CreatingAListenerTest extends AbstractEventTest {
 		)
 	}
 
-	@test.only()
+	@test()
 	protected static async emittingEventTriggersListenerAndCrashesWithListenerNotImplemented() {
 		const { currentSkill, skill2, eventContract, org } =
 			await this.setupSkillsInstallAtOrgRegisterEventContractAndGenerateListener(
